b019c4021c87ef255d6850830006faab49163935
EBIRIM <ebirima@EBIRIMs-MacBook-Pro.local>
2017-01-24 11:34:55 +0000

    Completed testing on filtering method and corrected array initialization error

--- spec/parliament/response_spec.rb
+++ spec/parliament/response_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../spec_helper'
+require 'pry'
 
 describe Parliament::Response, vcr: true do
-
   let(:nodes) { [] }
   subject { Parliament::Response.new(nodes) }
 
@@ -16,13 +16,25 @@ describe Parliament::Response, vcr: true do
       @response = Parliament::Request.new(base_url: 'http://localhost:3030').people.members.current.get
     end
 
-    it 'returns an array of arrays of objects filtered by type' do
-      filtered_response = @response.filter('http://id.ukpds.org/schema/Person', 'http://id.ukdps.org/schema/Party')
+    it 'returns an array of arrays of objects filtered by type Person' do
+      filtered_response = @response.filter('http://id.ukpds.org/schema/Person', 'http://id.ukpds.org/schema/Party')
 
-      # TODO: add more expects to check types of things
       expect(filtered_response.first.size).to eq(3)
+
       expect(filtered_response.size).to eq(2)
-      expect(filtered_response[1].size).to eq(3)
+
+      filtered_response.first.each do |node|
+        expect(node.type).to eq('http://id.ukpds.org/schema/Person')
+      end
+    end
+
+    it 'returns an array of arrays of objects filtered by type Party' do
+      filtered_response = @response.filter('http://id.ukpds.org/schema/Person', 'http://id.ukpds.org/schema/Party')
+      expect(filtered_response[1].size).to eq(1)
+
+      filtered_response[1].each do |node|
+        expect(node.type).to eq('http://id.ukpds.org/schema/Party')
+      end
     end
   end
 end
