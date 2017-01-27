b585ac3ae6ddc5ae7d17b3412ae1247b98b1d690
giusepped <giusdesan@gmail.com>
2017-01-27 15:51:39 +0000

    added missing tests for person and sitting decorators

--- spec/parliament/decorators/person_spec.rb
+++ spec/parliament/decorators/person_spec.rb
@@ -15,6 +15,7 @@ describe Parliament::Decorators::Person do
         person_node.extend(Parliament::Decorators::Person)
 
         expect(person_node.houses.size).to eq(1)
+        expect(person_node.houses.first.type).to eq('http://id.ukpds.org/schema/House')
       end
     end
 
@@ -24,6 +25,31 @@ describe Parliament::Decorators::Person do
         person_node.extend(Parliament::Decorators::Person)
 
         expect(person_node.houses).to eq([])
+      end
+    end
+  end
+
+  describe '#sittings' do
+    before(:each) do
+      @person_nodes = objects.select { |object| object.type == 'http://id.ukpds.org/schema/Person' }
+    end
+
+    context 'Grom::Node has all the required objects' do
+      it 'returns the houses for a Grom::Node objects of type Person' do
+        person_node = @person_nodes.first
+        person_node.extend(Parliament::Decorators::Person)
+
+        expect(person_node.sittings.size).to eq(1)
+        expect(person_node.sittings.first.type).to eq('http://id.ukpds.org/schema/Sitting')
+      end
+    end
+
+    context 'Grom::Node has no sittings' do
+      it 'returns an empty array' do
+        person_node = @person_nodes[2]
+        person_node.extend(Parliament::Decorators::Person)
+
+        expect(person_node.sittings).to eq([])
       end
     end
   end
