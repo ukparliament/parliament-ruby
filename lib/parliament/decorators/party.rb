ff8037c80e6f90c2a6678566419e6581f16f9be0
Rebecca Appleyard <rklappleyard@gmail.com>
2017-02-07 16:50:26 +0000

    Added party_memberships decorator to party

--- lib/parliament/decorators/party.rb
+++ lib/parliament/decorators/party.rb
@@ -5,6 +5,10 @@ module Parliament
       def name
         respond_to?(:partyName) ? partyName : ''
       end
+
+      def party_membership
+        respond_to?(:partyHasPartyMembership) ? partyHasPartyMembership : []
+      end
     end
   end
 end
