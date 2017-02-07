ed23355e13fd5f7d269c0f8518ec07a72bf9ed51
giusepped <giusdesan@gmail.com>
2017-02-07 17:02:05 +0000

    added full address method to postalAddress decorator

---
+++ lib/parliament/decorators/postal_address.rb
@@ -0,0 +1,24 @@
+module Parliament
+  module Decorators
+    module PostalAddress
+      def full_address
+        address_array.join("\n")
+      end
+
+      private
+
+      def address_array
+        address_array = []
+        for i in 1..5
+          if (respond_to?("addressLine#{i.to_s}".to_sym))
+            address_array << instance_variable_get("@addressLine#{i.to_s}".to_sym)
+          end
+        end
+        if respond_to?(:postCode)
+          address_array << postCode
+        end
+        address_array
+      end
+    end
+  end
+end
