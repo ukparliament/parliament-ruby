d8d800a3d365da278570b28f97962932bf48ca82
Rebecca Appleyard <rklappleyard@gmail.com>
2017-02-08 12:00:43 +0000

    Added seats and seat incumbencies decorators to house

--- lib/parliament/decorators/house.rb
+++ lib/parliament/decorators/house.rb
@@ -4,6 +4,21 @@ module Parliament
       def name
         respond_to?(:houseName) ? houseName : ''
       end
+
+      def seat_incumbencies
+        return @seat_incumbencies unless @seat_incumbencies.nil?
+
+        seat_incumbencies = []
+        seats.each do |seat|
+          seat_incumbencies << seat.seat_incumbencies
+        end
+
+        @seat_incumbencies = seat_incumbencies.flatten.uniq
+      end
+
+      def seats
+        respond_to?(:houseHasHouseSeat) ? houseHasHouseSeat : []
+      end
     end
   end
 end
