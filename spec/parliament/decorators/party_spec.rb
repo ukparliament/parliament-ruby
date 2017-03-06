563cc12dbb73069b01f1ace27add0fa2cb97d0d0
Rebecca Appleyard <rklappleyard@gmail.com>
2017-03-06 13:16:00 +0000

    Added decorator in party for member counts

--- spec/parliament/decorators/party_spec.rb
+++ spec/parliament/decorators/party_spec.rb
@@ -40,6 +40,7 @@ describe Parliament::Decorators::Party, vcr: true do
       response = Parliament::Request.new(base_url: 'http://localhost:3030').people(id).parties.get
       @party_nodes = response.filter('http://id.ukpds.org/schema/Party')
     end
+
     context 'Grom::Node has all the required objects' do
       it 'returns the party memberships for a Grom::Node object of type Party' do
         party_node = @party_nodes.first
@@ -54,6 +55,30 @@ describe Parliament::Decorators::Party, vcr: true do
         party_node = @party_nodes[1]
 
         expect(party_node.party_memberships).to eq([])
+      end
+    end
+  end
+
+  describe '#member_count' do
+    before(:each) do
+      id = '4b77dd58-f6ba-4121-b521-c8ad70465f52'
+      response = Parliament::Request.new(base_url: 'http://localhost:3030').houses(id).parties.current.get
+      @party_nodes = response.filter('http://id.ukpds.org/schema/Party')
+    end
+
+    context 'Grom::Node has a member count' do
+      it 'returns the member count for a Grom::Node object of type Party' do
+        party_node = @party_nodes.first
+
+        expect(party_node.member_count).to eq('2')
+      end
+    end
+
+    context 'Grom::Node has no count' do
+      it 'returns nil' do
+        party_node = @party_nodes[1]
+
+        expect(party_node.member_count).to be(nil)
       end
     end
   end
