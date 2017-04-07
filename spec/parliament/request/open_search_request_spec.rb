386ff3e160eec014d298d87802d16497c5986682
Rebecca Appleyard <rklappleyard@gmail.com>
2017-04-07 12:40:50 +0100

    Added testing around search parameters

--- spec/parliament/request/open_search_request_spec.rb
+++ spec/parliament/request/open_search_request_spec.rb
@@ -18,21 +18,38 @@ describe Parliament::Request::OpenSearchRequest, vcr: true do
 
   context '#get' do
     before(:each) do
-      request = Parliament::Request::OpenSearchRequest.new(base_url: 'http://parliament-search-api.azurewebsites.net/description',
+      @request = Parliament::Request::OpenSearchRequest.new(base_url: 'http://parliament-search-api.azurewebsites.net/description',
                                                             headers: { 'Accept' => 'application/atom+xml' },
                                                             builder: Parliament::Builder::OpenSearchResponseBuilder)
-      @result = request.get({ query: 'banana', start_page: '10' })
     end
 
     it 'returns a Feedjira Feed' do
-      expect(@result).to be_a(Feedjira::Parser::Atom)
+      result = @request.get({ query: 'banana', start_page: '10' })
+
+      expect(result).to be_a(Feedjira::Parser::Atom)
     end
 
     it 'returns the correct data within the Feedjira Feed' do
-      expect(@result.entries.first.title).to eq('House of Commons Standing Committee (pt 3)')
-      expect(@result.entries.first.summary).to include('Because <b>bananas</b> are the top-selling fruit')
-      expect(@result.entries.first.url).to eq('http://www.publications.parliament.uk/pa/cm199798/cmstand/euroa/st980325/80325s03.htm')
-      expect(@result.totalResults).to eq('18800')
+      result = @request.get({ query: 'banana', start_page: '10' })
+
+      expect(result.entries.first.title).to eq('House of Commons Standing Committee (pt 3)')
+      expect(result.entries.first.summary).to include('Because <b>bananas</b> are the top-selling fruit')
+      expect(result.entries.first.url).to eq('http://www.publications.parliament.uk/pa/cm199798/cmstand/euroa/st980325/80325s03.htm')
+      expect(result.totalResults).to eq('18800')
+    end
+
+    it 'sets the search parameters correctly - uses the defaults' do
+      @request.get({ query: 'banana' })
+
+      expect(WebMock).to have_requested(:get, 'http://parliament-search-api.azurewebsites.net/search?q=banana&start=1').
+          with(:headers => {'Accept'=>['*/*', 'application/atom+xml'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).once
+    end
+
+    it 'sets the search parameters correctly - uses the parameters passed in' do
+      @request.get({ query: 'banana', start_page: '20' })
+
+      expect(WebMock).to have_requested(:get, 'http://parliament-search-api.azurewebsites.net/search?q=banana&start=20').
+          with(:headers => {'Accept'=>['*/*', 'application/atom+xml'], 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).once
     end
   end
 end
