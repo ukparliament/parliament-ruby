8b81b04ca3e7ad5f6be4c59bb10892304905d897
Matt Rayner <mattrayner1@gmail.com>
2017-04-10 17:44:07 +0100

    Setup parliament-ruby for sinatra compatibilty

--- lib/parliament/request.rb
+++ lib/parliament/request.rb
@@ -2,7 +2,7 @@ module Parliament
   # Namespace for classes and modules that handle http connections.
   # @since 0.7.0
   module Request
-    Dir[File.join(File.dirname(__FILE__), '../parliament/request', '*.rb')].each { |request| require request }
-    # Currently just a namespace definition
+    require 'parliament/request/base_request'
+    require 'parliament/request/url_request'
   end
 end
